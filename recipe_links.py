7c5821ead35aa01cf3def9262c6fafe7129e23da
megankulshekar <91085343+megankulshekar@users.noreply.github.com>
2023-03-31 23:10:10 -0400

    refactoring links

--- recipe_links.py
+++ recipe_links.py
@@ -2,19 +2,12 @@ import bs4 as bs
 import urllib.request
 source = urllib.request.urlopen('https://www.allrecipes.com/cuisine-a-z-6740455').read()
 soup = bs.BeautifulSoup(source,'lxml')
-links = []
-for url in soup.find_all('a'):
+links = soup.find_all('a')
+for url in links:
     #print(url.get('href'))
-    if 'world-cuisine' in str(url.get('href')) or 'us-recipes' in str(url.get('href')):
-        links.append(str(url.get('href')))
-    #print(links)
-
-childs = {i:[] for i in links}
-for link in links:
-    source = urllib.request.urlopen(link).read()
-    soup = bs.BeautifulSoup(source,'lxml')
-    for url in soup.find_all('a'):
-        #print(url.get('href'))
-        if 'recipe/' in str(url.get('href')):
-            childs[link].append(str(url.get('href')))
-#print(childs)
+    if 'world-cuisine' in str(url.get("href")) or 'us-recipes' in str(url.get("href")):
+        source = urllib.request.urlopen(str(url.get("href"))).read()
+        soup = bs.BeautifulSoup(source,'lxml')
+        for url in soup.find_all('a'):
+            if 'recipe/' in str(url.get('href')):
+                print(str(url.get('href')))
