ef2b545d27511032c9a45032b9ae7d065c1cbf62
megankulshekar <91085343+megankulshekar@users.noreply.github.com>
2023-03-31 22:36:11 -0400

    added links of recipes from each cuisine

--- recipe_links.py
+++ recipe_links.py
@@ -4,7 +4,17 @@ source = urllib.request.urlopen('https://www.allrecipes.com/cuisine-a-z-6740455'
 soup = bs.BeautifulSoup(source,'lxml')
 links = []
 for url in soup.find_all('a'):
-    print(url.get('href'))
+    #print(url.get('href'))
     if 'world-cuisine' in str(url.get('href')) or 'us-recipes' in str(url.get('href')):
         links.append(str(url.get('href')))
-    print(links)
+    #print(links)
+
+childs = {i:[] for i in links}
+for link in links:
+    source = urllib.request.urlopen(link).read()
+    soup = bs.BeautifulSoup(source,'lxml')
+    for url in soup.find_all('a'):
+        #print(url.get('href'))
+        if 'recipe/' in str(url.get('href')):
+            childs[link].append(str(url.get('href')))
+#print(childs)
