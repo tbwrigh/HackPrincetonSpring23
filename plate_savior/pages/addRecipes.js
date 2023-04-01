13377388e1d0e6b828dd82c98e018d907b271716
Tyler <tyler.wright5146@gmail.com>
2023-04-01 09:00:04 -0400

    request

--- plate_savior/pages/addRecipes.js
+++ plate_savior/pages/addRecipes.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { AsyncStorage } from '@react-native-async-storage/async-storage';
 
 const AddRecipes = () => {
+  let recipes = [];
+  let cost, skill, time, allergens, cuisine;
+
+  useEffect(() => {
+    async function fetchData() {
+    console.log("fetch data begin")
+    // load values from AsyncStorage
+    cost = await AsyncStorage.getItem('price');
+    skill = await AsyncStorage.getItem('skillLevel');
+    time = await AsyncStorage.getItem('timeToCook');
+    allergens = await AsyncStorage.getItem('allergens');
+    cuisine = "tex-mex";
+
+    skill = skill == 1 ? "easy" : skill == 2 ? "medium" : skill == 3 ? "hard" : "expert";
+
+    console.log(cost, skill, time, allergens, cuisine);
+    // fetch recipes from api
+    // make get request to 10.25.13.105 
+    res = await fetch('http://10.25.13.105:3000/query/' + "$".repeat(cost) + "/" + skill + "/" + cuisine);
+    console.log(res);
+    recipes = await res.json();
+    }
+    console.log("fetching data")
+    fetchData();
+
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Add Recipes to Menu</Text>
+      <Text>{recipes}</Text>
     </View>
   );
 };
