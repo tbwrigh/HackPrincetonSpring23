6164d7010d4c15a4c7b1a670d61074aafc5d1384
megankulshekar <91085343+megankulshekar@users.noreply.github.com>
2023-04-01 17:26:47 -0400

    adding values to database

--- mongodb_api/main.py
+++ mongodb_api/main.py
@@ -58,3 +58,22 @@ def search_recipe(id: str):
     res = collection.find_one({"_id": ObjectId(id)})
     res['_id'] = str(res['_id'])
     return {"recipe": res}
+
+@app.get("/meal/{meal}")
+def search_type(meal: str):
+    if (meal == "apps"):
+        res = collection.find({"meal": { "$in": ['Appetizers and Snacks', 'Dips and Spreads Recipes']}})
+    elif (meal == "sides"):
+        res = collection.find({"meal": { "$in": ['Soup Recipes', 'Soups, Stews and Chili Recipes', 'Salad', 'Vegetable Soup Recipes', 'Side Dish', 'Yeast Bread Recipes', 'Quick Bread Recipes', 'Bread']}})
+    elif (meal == "breakfast"):
+        res = collection.find({"meal": { "$in": ['Breakfast and Brunch']}})
+    elif (meal == "lunch"):
+        res = collection.find({"meal": { "$in": ['Slow Cooker', 'Main Dishes', 'Beef', 'Pork', 'Seafood Main Dishes', 'Vegetables', 'Pasta and Noodles', 'Casserole Recipes', 'Fruits and Vegetables', 'Seafood', 'Breakfast and Brunch', 'BBQ & Grilling', 'Meat and Poultry', 'Everyday Cooking', 'Soups, Stews and Chili Recipes', 'Pasta', 'Chicken', 'Cuisine', 'African', 'U.S Recipes', 'Thai', 'European', 'Japanese', 'Asian', 'Mexican', 'Korean', 'Indian', 'Latin American', 'Chinese', 'Italian', 'Spanish', 'Cuisine']}})
+    elif (meal == "dinner"):
+        res = collection.find({"meal": { "$in": ['Slow Cooker', 'Main Dishes', 'Beef', 'Pork', 'Seafood Main Dishes', 'Vegetables', 'Pasta and Noodles', 'Casserole Recipes', 'Fruits and Vegetables', 'Seafood', 'Dinner', 'BBQ & Grilling', 'Meat and Poultry', 'Everyday Cooking', 'Soups, Stews, and Chili Recipes', 'Pasta', 'Chicken', 'Cuisine', 'Christmas', 'Thanksgiving', 'Holidays and Events Recipes', 'African', 'U.S Recipes', 'Thai', 'European', 'Japanese', 'Asian', 'Mexican', 'Korean', 'Indian', 'Latin American', 'Chinese', 'Italian', 'Spanish', 'Cuisine']}})
+    elif (meal == "desserts"):
+        res = collection.find({"meal": { "$in": ['Fruit Bread Recipes', 'Desserts', 'Pies', 'Cookies']}})
+    res = [r for r in res]
+    for r in res:
+        r['_id'] = str(r['_id'])
+    return {"recipes": res}
